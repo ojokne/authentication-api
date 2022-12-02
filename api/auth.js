53c087280d45c8eb4ab912c2d2c5fe52403c1d10
ojokne <ojokne@gmail.com>
2022-12-02 10:14:41 +0300

    Added password hashing
    Passwords are now hashed before storing in the database

--- api/auth.js
+++ api/auth.js
@@ -1,60 +1,90 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const db = require("../db");
 const models = require("../models/model");
-
-const SECRET_KEY = process.env.SECRET_KEY;
 const jwt = require("jsonwebtoken");
 
+const SECRET_KEY = process.env.SECRET_KEY;
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
 router.post("/signup", async (req, res) => {
-  const username = req.body.username.trim();
-  const password = req.body.password.trim();
   let message = "User created successfully";
   let status = 200;
-
-  if (username.length < 1 || password.length < 1) {
-    message = "Please fill in all fields";
-    status = 400;
-  } else {
-    let user = await models.User.findOne({
-      where: {
-        username: username,
-      },
-    });
-
-    if (user) {
-      message = "There is already a user with that username";
+  if (
+    req.body.hasOwnProperty("username") &&
+    req.body.hasOwnProperty("password")
+  ) {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username.length < 1 || password.length < 1) {
+      message = "Please populate all fields";
       status = 400;
     } else {
-      user = await models.User.create({
-        username: username,
-        password: password,
+      let user = await models.User.findOne({
+        where: {
+          username: username,
+        },
       });
+
+      if (user) {
+        message = "There is already a user with that username";
+        status = 400;
+      } else {
+        let passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+        user = await models.User.create({
+          username: username,
+          password: passwordHash,
+        });
+      }
     }
+  } else {
+    message = "Please include all fields";
   }
   res.status(status).json({ message: message });
 });
 
 router.post("/login", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  let token;
   let message = "success";
-  const user = await models.User.findOne({
-    where: {
-      username: username,
-    },
-  });
-  if (user && user.password == password) {
-    try {
-      token = jwt.sign({ username: username, password: password }, SECRET_KEY);
-    } catch (err) {
-      console.log(err);
+  let status = 200;
+  let token;
+  if (
+    req.body.hasOwnProperty("username") &&
+    req.body.hasOwnProperty("password")
+  ) {
+    const username = req.body.username.trim();
+    const password = req.body.password.trim();
+    if (username.length < 1 || password.length < 1) {
+      message = "Please populate all fields";
+      status = 400;
+    } else {
+      const user = await models.User.findOne({
+        where: {
+          username: username,
+        },
+      });
+      if (user) {
+        let passwordVerified = bcrypt.compare(password, user.password);
+        if (passwordVerified) {
+          try {
+            token = jwt.sign(
+              { username: username, password: password },
+              SECRET_KEY
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          message = "Incorrect username or password";
+        }
+      } else {
+        message = "No record found";
+      }
     }
   } else {
-    message = "Incorrect username or password";
+    message = "Please include all fields";
   }
-  res.status(200).json({ token: token, message: message });
+
+  res.status(status).json({ token: token, message: message });
 });
 
 module.exports = router;
