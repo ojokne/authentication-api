caa3c16af3693ab5ec77fc6dd700469fc34ed9af
ojokne <ojokne@gmail.com>
2023-01-04 21:27:45 +0300

    Add verify route handler

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const signup = require("./signup");
 const login = require("./login");
+const verify = require("./verify")
 
 const app = express.Router();
 
 app.use(signup);
 app.use(login);
+app.use(verify)
 
 module.exports = app;
