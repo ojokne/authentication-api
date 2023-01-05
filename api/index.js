26b500692afe3cf4ae4987c2ddd0e2533ff0505f
ojokne <ojokne@gmail.com>
2023-01-05 23:57:27 +0300

    Add request route handler

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const signup = require("./signup");
 const login = require("./login");
 const verify = require("./verify")
+const request = require("./request")
 
 const app = express.Router();
 
 app.use(signup);
 app.use(login);
 app.use(verify)
+app.use(request)
 
 module.exports = app;
